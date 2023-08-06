0d62c5b2c35d767fa017aa46d99a4aa48ee4a9b4
JoMiras <jonah@bayvalleytech.com>
2023-08-06 14:55:39 -0700

    Adding necessary files Needs more work, just only started

---
+++ Controllers/GameController.swift
@@ -0,0 +1,12 @@
+//
+//  GameController.swift
+//  ChessMess
+//
+//  Created by Jonah Mirasol on 2023/8/6.
+//
+
+import UIKit
+
+class GameViewController: UIViewController {
+    
+}
---
+++ Helpers/Extensions.swift
@@ -0,0 +1,8 @@
+//
+//  Extensions.swift
+//  ChessMess
+//
+//  Created by Jonah Mirasol on 2023/8/6.
+//
+
+import Foundation
