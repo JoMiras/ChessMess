ca965ec8bbdbef146de0ab1b927fd2ef6e008afc
JoMiras <jonah@bayvalleytech.com>
2023-08-06 14:55:39 -0700

    Adding necessary files Finished setting up board.swift

---
+++ Helpers/Constants.swift
@@ -0,0 +1,8 @@
+//
+//  Constants.swift
+//  ChessMess
+//
+//  Created by Jonah Mirasol on 2023/8/6.
+//
+
+import Foundation
---
+++ Models/Piece.swift
@@ -0,0 +1,8 @@
+//
+//  Piece.swift
+//  ChessMess
+//
+//  Created by Jonah Mirasol on 2023/8/6.
+//
+
+import Foundation
